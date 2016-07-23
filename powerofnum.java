93182d6e5cf197e7f77c58197059c7fe85412e58
vanmathivellaiyan <vanmathivellaiyan1995@gmail.com>
2016-07-23 11:48:57 +0530

    Add files via upload

---
+++ powerofnum.java
@@ -0,0 +1,13 @@
+package aa;
+
+public class powerofnum {
+	public static void main(String[] args){
+		int a=3,n = 0;
+		int sum=1;
+		for(int i=1;i<=4;i++){
+		sum=sum*a;
+		}
+		System.out.println(sum);
+	}
+
+}
